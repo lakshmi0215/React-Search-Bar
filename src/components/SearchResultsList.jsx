36916dbf46770d8cd0b1acb2fd7068b34c3beef1
Lakshmi_0215 <153126562+lakshmi0215@users.noreply.github.com>
2024-03-11 12:40:50 -0700

    Add files via upload

---
+++ src/components/SearchResultsList.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./SearchResultslist.css";
+import SearchResult from "./SearchResult";
+
+
+const SearchResultsList = ({results}) => {
+  return (
+  <div className="results-list"> 
+  {
+    results.map((result,id)=> 
+    {
+      return <SearchResult result={result.name} key={id}/>;
+    })}
+      </div>
+);
+  };
+
+export default SearchResultsList
